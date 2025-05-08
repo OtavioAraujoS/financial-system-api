f31f6021e7a7e42833933fc85a4408b7eea7936e
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-05-08 11:31:21 -0300

    feat: enable CORS and update server port configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+
+    await app.listen(process.env.PORT || 6669);
+
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
