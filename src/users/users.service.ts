0ebfbfb04aef4fe3d787a76a4c3babc1166b2018
OtavioAraujoS <otavioaraujo490@gmail.com>
2025-05-08 14:11:23 -0300

    feat: implement user service with CRUD operations

---
+++ src/users/users.service.ts
@@ -0,0 +1,76 @@
+// user.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) {}
+
+    /**
+     * Fetches all users from the database.
+     * @returns {Promise<User[]>} A promise that resolves to an array of users.
+     */
+    async getAllUsers() {
+        return this.userRepository.find();
+    }
+
+    /**
+     * Retrieves a user by ID.
+     * @param {string} id - The ID of the user.
+     * @returns {Promise<User>} A promise that resolves to the user data.
+     * @throws {NotFoundException} If the user is not found.
+     */
+    async getUserById(id: number) {
+        return this.userRepository.findOne({
+            where: { id },
+        });
+    }
+    /**
+     * Creates a new user.
+     * @param {Partial<User>} user - The user data to create.
+     * @returns {Promise<User>} A promise that resolves to the created user.
+     */
+    async createUser(user: Partial<User>) {
+        return this.userRepository.save(user);
+    }
+
+    /**
+     * Logs in a user.
+     * @param {Partial<User>} user - The user data to log in.
+     * @returns {Promise<User>} A promise that resolves to the logged-in user.
+     * @throws {Error} If the user is not found or password is incorrect.
+     */
+    async loginUser(user: Partial<User>) {
+        const foundUser = await this.userRepository.findOne({
+            where: { name: user.name, password: user.password },
+        });
+        if (!foundUser) {
+            throw new Error('User not found or incorrect password');
+        }
+        return foundUser;
+    }
+
+    /**
+     * Updates a user by ID.
+     * @param {number} id - The ID of the user to update.
+     * @param {Partial<User>} user - The user data to update.
+     * @returns {Promise<UpdateResult>} A promise that resolves to the update result.
+     */
+    async updateUser(id: number, user: Partial<User>) {
+        return this.userRepository.update(id, user);
+    }
+
+    /**
+     * Deletes a user by ID.
+     * @param {number} id - The ID of the user to delete.
+     * @returns {Promise<DeleteResult>} A promise that resolves to the delete result.
+     */
+    async deleteUser(id: number) {
+        return this.userRepository.delete(id);
+    }
+}
